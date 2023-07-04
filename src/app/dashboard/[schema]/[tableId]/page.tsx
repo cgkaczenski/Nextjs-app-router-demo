8277bcb9bb3f90cd89c44ecb82f07f3399d251f0
cgkaczenski <kzpulaski@gmail.com>
2023-07-04 16:19:38 -0500

    fix error on page reload

--- src/app/dashboard/[schema]/[tableId]/page.tsx
+++ src/app/dashboard/[schema]/[tableId]/page.tsx
@@ -1,49 +1,19 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import DataTable from "@/components/datatable";
-
-const addEditableFlag = (columns: any[]): any[] => {
-  return columns.map((column) => {
-    if (["first_name", "last_name"].includes(column.label)) {
-      return { ...column, isEditable: true };
-    }
-    return column;
-  });
-};
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { authOptions } from "../../../api/auth/[...nextauth]/route";
+import Table from "./table";
 
 export default async function TablePage({
   params,
 }: {
   params: { tableId: string };
 }) {
-  const router = useRouter();
-  const { data: session } = useSession();
+  const session = await getServerSession(authOptions);
+  const { tableId } = params;
 
   if (!session) {
-    router.push("/auth");
+    redirect("/auth");
   }
 
-  const { tableId } = params;
-  const response = await fetch(
-    process.env.BASE_URL + "/api/tables/" + tableId,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-cache",
-    }
-  );
-
-  const jsonData = await response.json();
-  if (jsonData.metadata.table_name === "user") {
-    jsonData.metadata.columns = addEditableFlag(jsonData.metadata.columns);
-  }
-  return (
-    session && (
-      <DataTable columns={jsonData.metadata.columns} data={jsonData.data} />
-    )
-  );
+  return <Table tableId={tableId} />;
 }
---
+++ src/app/dashboard/[schema]/[tableId]/table.tsx
@@ -0,0 +1,30 @@
+"use client";
+import DataTable from "@/components/datatable";
+
+const addEditableFlag = (columns: any[]): any[] => {
+  return columns.map((column) => {
+    if (["first_name", "last_name"].includes(column.label)) {
+      return { ...column, isEditable: true };
+    }
+    return column;
+  });
+};
+
+export default async function Table(props: any) {
+  const response = await fetch(
+    process.env.BASE_URL + "/api/tables/" + props.tableId,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-cache",
+    }
+  );
+
+  const jsonData = await response.json();
+  if (jsonData.metadata.table_name === "user") {
+    jsonData.metadata.columns = addEditableFlag(jsonData.metadata.columns);
+  }
+  return <DataTable columns={jsonData.metadata.columns} data={jsonData.data} />;
+}
