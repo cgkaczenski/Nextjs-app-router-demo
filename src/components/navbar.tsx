b695695c8f0dfe83881cfb350095ac508e8f536c
cgkaczenski <kzpulaski@gmail.com>
2023-07-04 18:12:46 -0500

    add hamburger menu and improve mobile phone style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,56 +3,79 @@
 import Link from "next/link";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Navbar() {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
 
   function logoutHandler() {
     signOut();
   }
 
+  const buttonClasses =
+    "rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75";
+
   return (
-    <header className="sticky top-0 w-full shadow-md flex bg-slate-50 h-20 justify-between items-center px-12">
-      <Link href="/" className="font-bold font-mono">
-        Next.js Demo
-      </Link>
-      <nav className="flex space-x-4">
-        {!session && (
-          <section>
-            <Link href="/auth" className="px-4">
-              Sign in
-            </Link>
-            <Link
-              href="/register"
-              className="rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-            >
-              Get started today
-            </Link>
-          </section>
-        )}
+    <header className="sticky top-0 w-full shadow-md flex bg-slate-50 h-20 justify-between items-center px-4 sm:px-12">
+      <div className="flex items-center space-x-4">
+        <Link href="/" className="font-bold font-mono">
+          Next.js Demo
+        </Link>
         {session && (
-          <>
-            <Link
-              href="/dashboard"
-              className="rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-            >
-              Dashboard
-            </Link>
-            <Link
-              href="/profile"
-              className="rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-            >
-              Profile
-            </Link>
-            <button
-              className="rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-              onClick={logoutHandler}
-            >
-              Logout
-            </button>
-          </>
+          <Link
+            href="/dashboard"
+            className={clsx(buttonClasses, "hidden sm:block")}
+          >
+            Dashboard
+          </Link>
         )}
-      </nav>
+      </div>
+      {!session && (
+        <section className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+          <Link href="/auth" className="px-4">
+            Sign in
+          </Link>
+          <Link href="/register" className={buttonClasses}>
+            Get started today
+          </Link>
+        </section>
+      )}
+      {session && (
+        <div>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
+              <path
+                fillRule="evenodd"
+                d="M4 5h12a1 1 0 010 2H4a1 1 0 110-2zm0 6h12a1 1 0 010 2H4a1 1 0 110-2zm0 6h12a1 1 0 010 2H4a1 1 0 110-2z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <nav
+            className={clsx(
+              "absolute right-0 flex flex-col items-start top-full mt-[-5px] w-auto bg-slate-50 shadow-md  space-y-4 p-4 rounded",
+              { hidden: !isOpen }
+            )}
+          >
+            <>
+              <Link
+                href="/dashboard"
+                className={clsx(buttonClasses, "sm:hidden")}
+              >
+                Dashboard
+              </Link>
+              <Link href="/profile" className={buttonClasses}>
+                Profile
+              </Link>
+              <button className={buttonClasses} onClick={logoutHandler}>
+                Logout
+              </button>
+            </>
+          </nav>
+        </div>
+      )}
     </header>
   );
 }
