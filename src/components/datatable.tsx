5294e3d23954ee22c9fdfafab380fe9020c8ed43
cgkaczenski <kzpulaski@gmail.com>
2023-07-06 23:51:41 -0500

    remove console log statement

--- src/components/datatable.tsx
+++ src/components/datatable.tsx
@@ -51,7 +51,6 @@ export default function DataTable(prop: {
   }
 
   async function handleSave() {
-    console.log("handleSave: ", localChanges);
     if (typeof onSave === "function") {
       const isSuccessful = await onSave(localChanges);
 
