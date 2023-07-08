0e82e849808fc9fb827b27cb3bca1b38b44c4c81
cgkaczenski <kzpulaski@gmail.com>
2023-07-07 20:33:48 -0500

    attempt #2 to fix error when reloading page

--- src/app/dashboard/[schema]/[tableId]/page.tsx
+++ src/app/dashboard/[schema]/[tableId]/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import { authOptions } from "../../../api/auth/[...nextauth]/route";
+import { cookies } from "next/headers";
 import Table from "./table";
 
 export default async function TablePage({
@@ -15,5 +16,33 @@ export default async function TablePage({
     redirect("/auth");
   }
 
-  return <Table tableId={tableId} />;
+  function addEditableFlag(columns: any[]) {
+    return columns.map((column) => {
+      if (["first_name", "last_name"].includes(column.label)) {
+        return { ...column, isEditable: true };
+      }
+      return column;
+    });
+  }
+
+  const response = await fetch(
+    process.env.BASE_URL + "/api/tables/" + tableId,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: cookies().toString(),
+      },
+      cache: "no-store",
+    }
+  );
+  if (!response.ok) {
+    return <div>error</div>;
+  }
+  let jsonData = await response.json();
+  if (jsonData.metadata.table_name === "user") {
+    jsonData.metadata.columns = addEditableFlag(jsonData.metadata.columns);
+  }
+
+  return <Table tableId={tableId} jsonData={jsonData} />;
 }
--- src/app/dashboard/[schema]/[tableId]/table.tsx
+++ src/app/dashboard/[schema]/[tableId]/table.tsx
@@ -3,28 +3,18 @@
 import DataTable from "@/components/datatable";
 import toast from "react-hot-toast";
 
-const addEditableFlag = (columns: any[]): any[] => {
-  return columns.map((column) => {
-    if (["first_name", "last_name"].includes(column.label)) {
-      return { ...column, isEditable: true };
-    }
-    return column;
-  });
-};
+export default async function Table(props: { tableId: string; jsonData: any }) {
+  const { jsonData } = props;
 
-export default async function Table(props: any) {
   async function handleSave(changes: Record<string, any>) {
-    const response = await fetch(
-      process.env.BASE_URL + "/api/tables/" + props.tableId,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        cache: "no-store",
-        body: JSON.stringify(changes),
-      }
-    );
+    const response = await fetch("/api/tables/" + props.tableId, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+      body: JSON.stringify(changes),
+    });
     const data = await response.json();
     if (!response.ok) {
       toast.error(data.error);
@@ -36,26 +26,13 @@ export default async function Table(props: any) {
     }
   }
 
-  const response = await fetch(
-    process.env.BASE_URL + "/api/tables/" + props.tableId,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-    }
-  );
-
-  const jsonData = await response.json();
-  if (jsonData.metadata.table_name === "user") {
-    jsonData.metadata.columns = addEditableFlag(jsonData.metadata.columns);
-  }
-  return (
+  return jsonData ? (
     <DataTable
       columns={jsonData.metadata.columns}
       data={jsonData.data}
       onSave={handleSave}
     />
+  ) : (
+    <div>Loading...</div>
   );
 }
